9b634246f58ce76649fd6317db245bcf970f76fb
Leandro <leoheck@gmail.com>
2019-09-20 01:20:25 -0300

    Update assoc test softwares

--- software/app/assoc_test/assoc_test.c
+++ software/app/assoc_test/assoc_test.c
@@ -1,11 +1,16 @@
 #include <hf-risc.h>
 #include "assoc.h"
 
-int main(void){
+// Test all peripherals
+
+int main(void) {
+
 	SYNC_MAC_OPER = 0xaabbccdd;
 	printf("SYNC_MAC_RESULT: %08x\n", SYNC_MAC_RESULT);
+
 	SYNC_MAC_OPER ^= -1;
 	ASYNC_MAC_OPER = 0xddbbccaa;
+
 	printf("ASYNC_MAC_RESULT: %08x\n", ASYNC_MAC_RESULT);
 	ASYNC_MAC_OPER ^= -1;
 	ASYNC_MAC_RST ^= -1;
